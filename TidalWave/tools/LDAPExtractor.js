09d3821290058d84cc09631a56cb1a8b4aa24f93
Jason Gauci <jgmath2000@gmail.com>
2015-01-13 21:17:50 -0800

    Make the UpdateLDAP tool generic.

---
+++ TidalWave/tools/LDAPExtractor.js
@@ -0,0 +1,7 @@
+exports.LDAPEntryToUser = function(entry) {
+  throw new Error("You must implement the functions in LDAPExtractor.js!");
+};
+
+exports.LDAPEntryToGroup = function(entry) {
+  throw new Error("You must implement the functions in LDAPExtractor.js!");
+};
