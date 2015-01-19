a7cce5d72d8badfe01ed9cf7f268561480fcdf97
Jason Gauci <jgauci@apple.com>
2015-01-19 11:26:34 -0800

    add options file as command-line parameter.

--- server/options-handler.js
+++ server/options-handler.js
@@ -1,6 +1,7 @@
 var fs = require('fs');
 var path = require('path');
+var argv = require('optimist').argv;
 
 // Load options into JS object.
-exports.options = JSON.parse(fs.readFileSync(path.resolve(__dirname,'../options.json')));
+exports.options = JSON.parse(fs.readFileSync(path.resolve(__dirname,'../'+argv.optionsFile)));
 
