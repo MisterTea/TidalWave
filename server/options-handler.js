8b17519bcd20f691f94019be636eb474f6a1f706
Jason Gauci <jgmath2000@gmail.com>
2015-01-28 16:47:11 -0800

    Change options handler to use filepath instead of filename.

--- server/options-handler.js
+++ server/options-handler.js
@@ -3,5 +3,5 @@ var path = require('path');
 var argv = require('optimist').argv;
 
 // Load options into JS object.
-exports.options = JSON.parse(fs.readFileSync(path.resolve(__dirname,'../'+argv.optionsFile)));
+exports.options = JSON.parse(fs.readFileSync(argv.optionsFile));
 
